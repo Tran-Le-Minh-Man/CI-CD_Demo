58cb68da28115f70640727d569805857b2929df5
ADMIN <ADMIN@LAPTOP-3C3PE2GE>
2026-04-10 08:04:05 +0700

    Commit junit and pom.xml

---
+++ sonar-demo/src/test/java/TestCalculator.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCalculator {
+
+	@Test
+	void test() {
+		Calculator c = new Calculator();
+		assertEquals(15, c.plus(10, 5));
+	}
+
+}
